cfd6dfc3e31bd638e0af390009421d08e2da4d1c
Craig <craig@RedHpDebian>
2019-12-05 21:40:45 +0000

    mend

--- C#/SolarLunarName/SolarLunarName.Standard/ApplicationServices/DateInterpreter.cs
+++ C#/SolarLunarName/SolarLunarName.Standard/ApplicationServices/DateInterpreter.cs
@@ -44,6 +44,42 @@ namespace SolarLunarName.Standard.ApplicationServices
         }
 
 
+    
+        public Models.SolarLunarName GetRemoteSolarLunarName(DateTime solarDateTime)
+            {
+
+                var year = solarDateTime.Year;
+                var startOfYear = new DateTime(year, 1, 1);
+                var db = new RestServices.RemoteJson.RemoteMoonDataClient();
+
+                var newMoons = db.GetYear(year.ToString()).Where(
+                                    x => x.Date > startOfYear
+                                    && x.Date < solarDateTime
+                                    && x.Phase == Moon.MoonPhase.NewMoon
+
+                                )
+                                .OrderBy(x => x.Date);
+
+                var lunarMonth = newMoons.Count();
+
+                int lunarDay;
+                if (newMoons.Any())
+                {
+                    var dayOfNewMoon = newMoons.Last().Date.DayOfYear;
+                    lunarDay = solarDateTime.DayOfYear - dayOfNewMoon;
+                }
+                else
+                {
+                    lunarDay = solarDateTime.DayOfYear;
+                }
+
+                return new Models.SolarLunarName(solarDateTime, year, lunarMonth, lunarDay);
+
+                
+            }
+
+
     }
+    
 }
 
