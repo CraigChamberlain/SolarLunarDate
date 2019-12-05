6ae79aee8f24618e6732f6588483df051ff79f40
Craig <craig@RedHpDebian>
2019-12-05 21:36:10 +0000

    Remote json handler without restsharp dependency

--- C#/SolarLunarName/SolarLunarName.Standard/RestServices/RemoteJson.cs
+++ C#/SolarLunarName/SolarLunarName.Standard/RestServices/RemoteJson.cs
@@ -1,15 +1,21 @@
 using System.Collections.Generic;
+using System.Net;
 using Newtonsoft.Json;
 using SolarLunarName.Standard.Models;
 
-namespace SolarLunarName.Standard.RestServices.LocalJson{
 
-    public class  MoonDataClient
+namespace SolarLunarName.Standard.RestServices.RemoteJson{
+
+    public class  RemoteMoonDataClient
     {
 
-            public List<MoonPhaseEntity> GetYear(string path){
-                var yearJson = System.IO.File.ReadAllText(path);
+        const string BaseUrl = "https://raw.githubusercontent.com/CraigChamberlain/SolarLunarDate/master/docs/api/moon-data/";
+        
+        public List<MoonPhaseEntity> GetYear(string year){
+            using(var client = new WebClient()){
+                var yearJson = client.DownloadString(BaseUrl+year+".json");
                 return JsonConvert.DeserializeObject<List<MoonPhaseEntity>>(yearJson);
             }
+        }
     }
 }
