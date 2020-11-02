78bb1bfb33edd4c21f788c52ed6d571a833895c2
Craig Chamberlain <craig.chamberlain11@gmail.com>
2020-11-02 20:47:30 +0000

    Add tool for automating Test Coverage

---
+++ C#/SolarLunarName/CreateTestCoverage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+COVERALLS_REPO_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxx
+
+find . -path "*/TestResults/*" -exec rm "{}" -r \;
+
+find . -path "*.Tests" -exec dotnet test --collect:"XPlat Code Coverage" "{}" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover  \;
+
+
+COVERAGEFILES=""
+for x in $(find . -path */coverage.opencover.xml); do
+    COVERAGEFILES+="opencover=$x;"
+done
+COVERAGEFILES="$(sed -e 's/;$//' <<< $COVERAGEFILES)"
+
+csmacnz.Coveralls \
+  --multiple \
+  -i $COVERAGEFILES \
+  --commitId $(git log --format="%H" -n 1) \
+  --commitBranch $(git rev-parse --abbrev-ref HEAD)
