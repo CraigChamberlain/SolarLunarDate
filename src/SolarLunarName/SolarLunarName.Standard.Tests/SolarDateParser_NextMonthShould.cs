82dd9dd31c82d894077bae763e49943d8f6cfe3a
Craig Chamberlain <craig.chamberlain11@gmail.com>
2020-11-04 17:42:35 +0000

    Mock ISolarLunarCalendarClient

--- src/SolarLunarName/SolarLunarName.Standard.Tests/SolarDateParser_NextMonthShould.cs
+++ src/SolarLunarName/SolarLunarName.Standard.Tests/SolarDateParser_NextMonthShould.cs
@@ -1,20 +1,44 @@
 using Xunit;
 using SolarLunarName.Standard.ApplicationServices;
-using SolarLunarName.Standard.RestServices.LocalJson;
 using SolarLunarName.SharedTypes.Models;
 using SolarLunarName.SharedTypes.Exceptions;
+using SolarLunarName.SharedTypes.Interfaces;
+using System.Collections.Generic;
+using System;
 
 namespace SolarLunarName.Standard.Tests
-{
+{   public class MockClient: ISolarLunarCalendarClient
+    {
+        public IList<ILunarSolarCalendarMonth> GetYearData(string year){
+            
+            return new List<ILunarSolarCalendarMonth>{
+                new LunarSolarCalendarMonth(19, new DateTime(1700,1,1)),
+                new LunarSolarCalendarMonth(29,new DateTime(1700,01,20)),
+                new LunarSolarCalendarMonth(30,new DateTime(1700,02,18)),
+                new LunarSolarCalendarMonth(30,new DateTime(1700,03,20)),
+                new LunarSolarCalendarMonth(29,new DateTime(1700,04,19)),
+                new LunarSolarCalendarMonth(30,new DateTime(1700,05,18)),
+                new LunarSolarCalendarMonth(29,new DateTime(1700,06,17)),
+                new LunarSolarCalendarMonth(29,new DateTime(1700,07,16)),
+                new LunarSolarCalendarMonth(30,new DateTime(1700,08,14)),
+                new LunarSolarCalendarMonth(29,new DateTime(1700,09,13)),
+                new LunarSolarCalendarMonth(29,new DateTime(1700,10,12)),
+                new LunarSolarCalendarMonth(30,new DateTime(1700,11,10)),
+                new LunarSolarCalendarMonth(22,new DateTime(1700,12,10))
+            };
+        }
+        public ILunarSolarCalendarMonth GetMonthData(int year, int month)=> throw new NotImplementedException();
+        public IList<DateTime> GetYear(string year)=> throw new NotImplementedException();
+    }
     public class SolarDateParser_NextMonthShould
     {   
         private SolarDateParser dp;
+        private ISolarLunarCalendarClient _client;
+        
 
-        //TODO
-        // Should be mocking LunarCalendarClient
         public SolarDateParser_NextMonthShould(){
-
-            dp = new SolarDateParser(new LunarCalendarClient(Paths.calendarApi));
+            _client = new MockClient();
+            dp = new SolarDateParser(_client);
 
         }
 
